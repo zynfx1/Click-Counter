57ab1be0e337cef5e8a09477e9c93e130790bae5
zynfx1 <sebigan.zyn@gmail.com>
2025-12-04 22:10:24 +0800

    add/minBtn func

--- src/script.js
+++ src/script.js
@@ -1,10 +1,36 @@
 const btn = document.querySelector(".btn")
 const counter = document.querySelector(".counter")
+const addBtn = document.querySelector(".addBtn")
+const minBtn = document.querySelector(".minBtn")
 
 let count = 0;
+
+
+
 
 btn.addEventListener("click", ()=>{
     count++;
     counter.textContent = count;
 })
 
+addBtn.addEventListener("click", ()=> {
+    count++;
+    counter.textContent = count;
+})
+
+minBtn.addEventListener("click", ()=> {
+    count--;
+    counter.textContent = count;
+
+    if(count<0){
+        minBtn.disabled = true;
+        counter.textContent = 0;
+        console.log("Error")
+      
+    }
+    else
+    {
+
+       
+    }
+})
